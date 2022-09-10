a0cfab3c0d1ac11a3d9f4db5d7549e6b8058eae6
merthamit <merthamit11@gmail.com>
2022-09-10 11:27:32 +0300

    new

--- leetcodes questions/500.py
+++ leetcodes questions/500.py
@@ -1,29 +1,26 @@
 # Benim çözdüğüm
-# Çözüm sayısı 0 | Hedef 5 çözüm
+# Çözüm sayısı 1 | Hedef 5 çözüm
 class Solution(object):
     def findWords(self, words):
-        a = set('asdfghjkl')
-        b = set('zxcvbnm')
-        c = set('qwertyuiop')
+        a = set('asdfghjklASDFGHJKL')
+        b = set('zxcvbnmZXCVBNM')
+        c = set('qwertyuiopQWERTYUIOP')
         result = []
-
         for i in words:
-            if i[0].lower() in a:
+            firstChar = i[0]
+            if firstChar in a:
                 if self.isAll(i, a): result.append(i)
-            if i[0].lower() in b:
+            elif firstChar in b:
                 if self.isAll(i, b): result.append(i)
-            if i[0].lower() in c:
+            elif firstChar in c:
                 if self.isAll(i, c): result.append(i)
-        
         return result
 
-
-
-    def isAll(self, word, alp):
-        for i in word.lower():
-            if i not in alp:
-                return False
+    def isAll(self, s, keyboard):
+        for i in s:
+            if i not in keyboard: return False
         return True
+        
 
 print(Solution().findWords(['a','b']))
 print(Solution().findWords(["Hello","Alaska","Dad","Peace"]))
