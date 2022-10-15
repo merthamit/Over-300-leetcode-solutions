61ee40aa68da426f254bf110540677edc0c7a12d
merthamit <merthamit11@gmail.com>
2022-10-15 22:39:17 +0300

    a

--- leetcodes questions/322.py
+++ leetcodes questions/322.py
@@ -20,21 +20,18 @@ class Solution(object):
         return -1
                     
                 
-        
 class Solution(object):
     def coinChange(self, coins, amount):
-        if amount <= 0: return 0
-        q = collections.deque()
-        for i in coins:
-            q.append((i, 1))
-        visit = set()
-        
-        while q:
-            coin, count = q.popleft()
-            if coin == amount: return count
-            for i in coins:
-                total = coin + i
-                if total not in visit and amount >= total:
-                    visit.add(total)
-                    q.append((total, count+1))
-        return -1
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], dp[a - c] + 1)
+
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+print(Solution().coinChange([1,3,4,5], 7))
