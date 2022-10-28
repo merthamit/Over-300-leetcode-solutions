21ac21b99df5f91468a0cadbf71c716af975294a
merthamit <merthamit11@gmail.com>
2022-10-28 13:36:33 +0300

    543

---
+++ leetcodes questions/543.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        if not root: return 0
+        output = 0
+        def dfs(root, depth):
+            if not root: return 0
+            if not root.left and not root.right:
+                return depth
+            
+            return max(dfs(root.left, depth + 1),dfs(root.right, depth + 1))
+
+        stack = [(root, 0)]
+        
+        while stack:
+            node, depth = stack.pop()
+            l, r = 0, 0
+            
+            if node.left:
+                stack.append((node.left, depth + 1))
+                l = dfs(node.left, 1)
+            if node.right:
+                stack.append((node.right, depth + 1))
+                r = dfs(node.right, 1)
+            
+            output = max(output, l + r)
+
+                
+        return output
+
+
+# Adamın çözdüğü
+# Çözüm sayısı 0 | Hedef 5 çözüm
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        res = [0]
+        
+        def dfs(root):
+            if not root: return -1
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            res[0] = max(res[0], left + right + 2)
+            return 1 + max(left, right)
+        
+        dfs(root)
+        
+        return res[0]
+    
+            
