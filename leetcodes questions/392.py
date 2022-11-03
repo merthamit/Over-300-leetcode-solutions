935614026cd134f76e98da501d9150072e1d12e8
merthamit <merthamit11@gmail.com>
2022-11-03 12:19:51 +0300

    392

---
+++ leetcodes questions/392.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        i, j = 0, 0
+        
+        while len(s) > i and len(t) > j:
+            if len(s) > i and s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return len(s) == i
+            
