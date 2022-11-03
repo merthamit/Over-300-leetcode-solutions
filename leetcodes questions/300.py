647edd5bb494f56fbb53a4b64e908f6b392ff483
merthamit <merthamit11@gmail.com>
2022-11-03 13:55:12 +0300

    300

---
+++ leetcodes questions/300.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        dp = [1] * len(nums)
+        
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                       
+        return max(dp)
