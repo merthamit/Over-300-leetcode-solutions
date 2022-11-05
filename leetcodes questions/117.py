6b32e8e94a13af13f8cdcd16c7edfd8af42bf8c4
merthamit <merthamit11@gmail.com>
2022-11-05 18:59:20 +0300

    117 U

--- leetcodes questions/117.py
+++ leetcodes questions/117.py
@@ -24,6 +24,32 @@ class Solution(object):
             curr = dummy.next
             
         return root
+
+# YENİ BENİM ÇÖZDÜĞÜM
+class Solution(object):
+    def connect(self, root):
+        if not root: return root
+        
+        curr = root
+        pre = Node()
+        dummy = None
+        while curr:
+            if curr.left:
+                pre.next = curr.left
+                pre = pre.next
+                dummy = curr.left if not dummy else dummy
+            if curr.right:
+                pre.next = curr.right
+                pre = pre.next
+                dummy = curr.right if not dummy else dummy
+                
+            curr = curr.next
+            if not curr:
+                pre = Node()
+                curr = dummy
+                dummy = None
+                
+        return root
             
         
             
