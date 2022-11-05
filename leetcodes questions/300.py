ef3af72fb986115d87e08888f810c7f489976980
merthamit <merthamit11@gmail.com>
2022-11-05 03:32:35 +0300

    300 U

--- leetcodes questions/300.py
+++ leetcodes questions/300.py
@@ -1,10 +1,12 @@
 from bisect import bisect_left
 
 
+# Adamın çözdüğü
+# Çözüm sayısı 0 | Hedef 5 çözüm 
 class Solution(object):
     def lengthOfLIS(self, nums):
         dp = [1] * len(nums)
-        
+        # BURAYI YAPTIN YARIN DİĞERİNİ ANLA YAP @@@@@@@@@@@@@@
         for i in range(len(nums)-1, -1, -1):
             for j in range(i+1, len(nums)):
                 if nums[i] < nums[j]:
