7ac348796f65dafb5cfc0f837d68546dc6090c80
merthamit <merthamit11@gmail.com>
2022-11-07 01:42:56 +0300

    605

---
+++ leetcodes questions/605.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i] == 0 and flowerbed[i+1] == 0 and flowerbed[i-1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        
+        return n <= 0
