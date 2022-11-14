08e2295e505285c186f94a1ba7d2ab3327f6ad7c
merthamit <merthamit11@gmail.com>
2022-11-14 05:14:57 +0300

    74 u

--- leetcodes questions/74.py
+++ leetcodes questions/74.py
@@ -56,6 +56,22 @@ class Solution(object):
         
         return False
 
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        row, col = ROWS-1, 0
+        
+        while COLS > col and row >= 0:
+            if matrix[row][col] < target:
+                col += 1
+            elif matrix[row][col] > target:
+                row -= 1
+            else:
+                return True
+            
+        return False
+                
 
 print(Solution().searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],7))
 
