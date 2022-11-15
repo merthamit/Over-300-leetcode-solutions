abdfca87be9ea08c264ca1e6ef7e638df578867e
merthamit <merthamit11@gmail.com>
2022-11-15 22:52:36 +0300

    90

---
+++ leetcodes questions/90.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        res = []
+        nums.sort()
+        
+        
+        def backTrack(i, subSet):
+            if i == len(nums):
+                res.append(subSet[:])
+                return
+            
+            subSet.append(nums[i])
+            backTrack(i+1, subSet)
+            subSet.pop()
+            
+            while i + 1 < len(nums) and nums[i+1] == nums[i]:
+                i += 1
+                
+            backTrack(i+1, subSet)
+            
+            
+            
+        backTrack(0, [])
+        return res
