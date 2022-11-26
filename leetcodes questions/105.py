bb9941c294b6a588ec497d9fce488795dc069564
merthamit <merthamit11@gmail.com>
2022-11-26 20:13:21 +0300

    105

---
+++ leetcodes questions/105.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder: return
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1: mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        
+        return root
+        
+        
+        
+3, 9, 1, 2, 20, 15, 17
+
+1, 9, 2, 3, 15, 20, 7
