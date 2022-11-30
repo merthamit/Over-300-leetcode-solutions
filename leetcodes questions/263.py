be752318b4b9e3c00e0a86edc7f94fd0395d8c9e
merthamit <merthamit11@gmail.com>
2022-11-30 04:20:20 +0300

    263

---
+++ leetcodes questions/263.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n <= 0: return False
+        
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n //= p
+            
+        return n == 1
+        
