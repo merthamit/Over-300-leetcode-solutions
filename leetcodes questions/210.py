3b151ce735485da976fae1a7f4f634678cf41273
merthamit <merthamit11@gmail.com>
2022-12-04 22:26:10 +0300

    210

---
+++ leetcodes questions/210.py
@@ -0,0 +1,60 @@
+import collections
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        if numCourses <= 1: return [0]
+        preMap = collections.defaultdict(list)
+        res = []
+        for a, b in prerequisites:
+            preMap[a].append(b)
+        
+        cycle, courseQ = set(), set()
+        def dfs(crs):
+            if crs in cycle: return False
+            if not preMap[crs]: 
+                if crs not in courseQ:
+                    courseQ.add(crs)
+                    res.append(crs)
+                return True
+            
+            cycle.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            cycle.remove(crs)
+            res.append(crs)
+            courseQ.add(crs)
+            preMap[crs] = []
+            
+            
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i): return []
+            
+        return res
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        preMap = collections.defaultdict(list)
+        res = []
+        for a, b in prerequisites:
+            preMap[a].append(b)
+        
+        cycle, visit = set(), set()
+        def dfs(crs):
+            if crs in cycle: return False
+            if crs in visit: return True
+            
+            cycle.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            cycle.remove(crs)
+            res.append(crs)
+            visit.add(crs)
+            
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i): return []
+            
+        return res
