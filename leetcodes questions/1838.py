eaea099aa25dff52daede6aaaec1a7701c818303
merthamit <merthamit11@gmail.com>
2022-12-05 22:51:42 +0300

    1838

---
+++ leetcodes questions/1838.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxFrequency(self, nums, k):
+        nums.sort()
+        l = total = maxLen = 0
+        
+        for r in range(len(nums)):
+            total += nums[r]
+            
+            while nums[r] * (r - l + 1) > total + k:
+                total -= nums[l]
+                l += 1
+            
+            maxLen = max(maxLen, (r - l + 1))
+        
+        return maxLen
