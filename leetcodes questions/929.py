50d6ca08a505261df7ee0a6950eb017f851f9b04
merthamit <merthamit11@gmail.com>
2022-12-05 23:21:19 +0300

    929

---
+++ leetcodes questions/929.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        emailsHash, res = {}, 0
+        
+        for email in emails:
+            splitEmail = email.split('@') 
+            localName = splitEmail[0].split('+')[0].replace('.','')
+            domainName = splitEmail[1]
+            cleanEmail = localName + '@' + domainName
+            emailsHash[(cleanEmail)] = 1 + emailsHash.get(cleanEmail, 0)
+            
+        return len(emailsHash)
+
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        emailsHash, res = set(), 0
+        
+        for email in emails:
+            i, local = 0, ''
+            while email[i] not in ['@', '+']:
+                if email[i] != '.':
+                    local += email[i]
+                i += 1
+            
+            while email[i] != '@':
+                i += 1
+            
+            domain = email[i+1:]
+            emailsHash.add((local, domain))
+                
+            
+        return len(emailsHash)
