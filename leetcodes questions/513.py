024ebead13d15c8329591b11a8897514ed00efb6
merthamit <merthamit11@gmail.com>
2022-12-05 03:53:06 +0300

    513

---
+++ leetcodes questions/513.py
@@ -0,0 +1,17 @@
+import collections
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        q = collections.deque([root])
+        
+        while q:
+            node = q.popleft()
+            if node.right: q.append(node.right)
+            if node.left: q.append(node.left)
+             
+        return node.val
