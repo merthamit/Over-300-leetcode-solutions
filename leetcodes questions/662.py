6c5c76a4d1941dbad06aa682dbc0abdad3d13ddb
merthamit <merthamit11@gmail.com>
2022-12-10 04:41:48 +0300

    662

---
+++ leetcodes questions/662.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import collections
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        q = collections.deque([(root, 1)])
+        width = 0
+        
+        while q:
+            _,left = q[0]
+            _,right =  q[-1]
+            width = max(width, right - left + 1)
+            lng = len(q)
+            print(left, right)
+            
+            for _ in range(lng):
+                node, idx = q.popleft()
+                
+                if node.left:
+                    q.append((node.left, idx*2))
+                if node.right:
+                    q.append((node.right, idx*2+1))
+            
+                        
+        return width
+        
+        
+        
+        
