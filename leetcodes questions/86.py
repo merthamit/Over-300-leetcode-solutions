b1aa82b7e89f2946580adf2828922c4bc90c0d43
merthamit <merthamit11@gmail.com>
2022-12-13 00:06:40 +0300

    86

---
+++ leetcodes questions/86.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        left = ltail = ListNode()
+        right = rtail = ListNode()
+        curr = head
+        
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            
+            head = head.next
+        
+        ltail.next = right.next
+        rtail.next = None   
+        
+        return left.next
+        
+        
