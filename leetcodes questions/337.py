39bd97d8e80333ebcc46cc21d2f7c9ee0e4b33a3
merthamit <merthamit11@gmail.com>
2022-12-16 05:44:12 +0300

    337

---
+++ leetcodes questions/337.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rob(self, root):
+        
+        def dfs(root):
+            if not root: return [0, 0]
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            withRoot = root.val + left[1] + right[1]
+            withOutRoot = max(left) + max(right)
+            
+            return [withRoot, withOutRoot]
+        
+        return max(dfs(root))
+            
+            
