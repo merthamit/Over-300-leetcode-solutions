c087758c610290dde8529123988a6a213ed08740
merthamit <merthamit11@gmail.com>
2022-12-20 17:03:20 +0300

    784 u

--- leetcodes questions/784.py
+++ leetcodes questions/784.py
@@ -15,3 +15,23 @@ class Solution(object):
         
         dfs(0, s)
         return res
+
+class Solution(object):
+    def letterCasePermutation(self, s):
+        if not s: return []
+        res = set()
+        
+        def dfs(i, curr):
+            if len(s) == i:
+                res.add(curr[:])
+                return
+            
+            newS = curr[:i] + curr[i].swapcase() + curr[i+1:]
+            dfs(i+1, newS) 
+            newS = curr
+            dfs(i+1, newS)
+        
+        dfs(0, s)
+        return res
+            
+        
