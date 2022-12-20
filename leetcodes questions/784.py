c9c0558b9187c6fc3ef89c26bc036f78bdd8334d
merthamit <merthamit11@gmail.com>
2022-12-20 16:56:32 +0300

    784

---
+++ leetcodes questions/784.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def letterCasePermutation(self, s):
+        if not s: return []
+        res = set()
+        
+        def dfs(i, curr):
+            if len(s) == i:
+                res.add(curr[:])
+                return
+            
+            newS = curr[:i] + curr[i].upper() + curr[i+1:]
+            dfs(i+1, newS) 
+            newS = curr[:i] + curr[i].lower() + curr[i+1:]
+            dfs(i+1, newS)
+        
+        dfs(0, s)
+        return res
