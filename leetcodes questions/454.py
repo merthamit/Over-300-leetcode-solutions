22184e8b6628523792a2acb6c9609594bd6a5601
merthamit <merthamit11@gmail.com>
2022-12-30 01:02:56 +0300

    454

--- leetcodes questions/454.py
+++ leetcodes questions/454.py
@@ -19,6 +19,8 @@ class Solution(object):
 # 0 <= i, j, k, l < n
 # nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
  
- 
+# Eşitlikte herhangi bir elemanı sağa veya sola attığında karşıdaki elemanlardan yine sağa ve sola attığında eşitlik değişmez 50 + 30 = 40 + 40
+# -40 + 30 = -50 + 40
+# yine aynı bu problem tamamen budur
 
 print(Solution().fourSumCount([1, 2], [-2, -1], [-1, 2], [0, 2]))
