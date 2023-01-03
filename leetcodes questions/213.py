1a04d2694185f2afd4f55b79d794ac92a4935014
merthamit <merthamit11@gmail.com>
2023-01-03 05:30:32 +0300

    213

---
+++ leetcodes questions/213.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob(self, nums):
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+        
+        
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+        
+        for n in nums:
+            tmp = max(rob1+n, rob2)
+            rob1 = rob2
+            rob2 = tmp
+            
+        return rob2
+            
+            
