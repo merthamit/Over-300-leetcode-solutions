ab26dc718c6cef30ab4154965a6ab7b9d670e9ae
merthamit <merthamit11@gmail.com>
2023-01-03 05:15:15 +0300

    46

---
+++ leetcodes questions/46.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        res = []
+        
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            
+            for perm in perms:
+                perm.append(n)
+                
+            res.extend(perms)
+            nums.append(n)
+            
+        return res
+        
