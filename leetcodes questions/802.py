d20e8d45f6e3f7819c2af6f9cb18c9ed4217a602
merthamit <merthamit11@gmail.com>
2023-01-03 03:56:45 +0300

    802

--- leetcodes questions/802.py
+++ leetcodes questions/802.py
@@ -1,4 +1,29 @@
 import collections
+
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        n = len(graph)
+        res, safe = [], {}
+        
+        def dfs(i):
+            if i in safe: return safe[i]
+            safe[i] = False
+            
+            for nei in graph[i]:
+                if not dfs(nei): return safe[i]
+                
+            safe[i] = True
+            return safe[i]
+            
+        
+        for i in range(n):
+            if dfs(i): res.append(i)
+        
+        return res
+            
+            
+            
+
 class Solution(object):
     def eventualSafeNodes(self, graph):
         graphList = collections.defaultdict(list)
