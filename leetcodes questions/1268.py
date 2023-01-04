b3c49241d7200955db93da14dea5516dc19d51fe
merthamit <merthamit11@gmail.com>
2023-01-04 04:03:37 +0300

    1268

---
+++ leetcodes questions/1268.py
@@ -0,0 +1,46 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+        self.futureWords = []
+        
+    def addWord(self, word):
+        curr = self
+        curr.futureWords.append(word)
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+            curr.futureWords.append(word)
+
+        
+        curr.endOfWord = True
+        curr.fullyWord = word
+
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        root = TrieNode()
+        res = []
+        
+        for product in products:
+            root.addWord(product)
+        
+        finish = False
+        for s in searchWord:
+            if s not in root.children or finish:
+                res.append([])
+                finish = True
+                continue
+            root = root.children[s]
+            k, subres = 3, []
+            root.futureWords.sort()
+            for i in root.futureWords:
+                if k == 0: break
+                subres.append(i)
+                k -= 1
+                
+            res.append(sorted(subres))
+            
+        return res
+        
