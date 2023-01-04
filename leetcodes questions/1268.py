e75ccd735f141af0dc880309b9b6e872931e9e86
merthamit <merthamit11@gmail.com>
2023-01-04 04:57:02 +0300

    1268u

--- leetcodes questions/1268.py
+++ leetcodes questions/1268.py
@@ -43,4 +43,24 @@ class Solution(object):
             res.append(sorted(subres))
             
         return res
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        products.sort()
+        res = []
         
+        l, r = 0, len(products) - 1
+        
+        for i in range(len(searchWord)):
+            c = searchWord[i]
+            
+            while r >= l and (len(products[l]) <= i or products[l][i] != c): l += 1
+            while r >= l and (len(products[r]) <= i or products[r][i] != c): r -= 1
+
+            
+            res.append([])
+            remain = r - l + 1
+            for j in range(min(3, remain)):
+                res[-1].append(products[l+j])
+        
+        return res
