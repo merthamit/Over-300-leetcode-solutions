adbf576f7c7beb590941f49065f166d8c00dd0e1
merthamit <merthamit11@gmail.com>
2023-01-05 22:24:52 +0300

    71 u

--- leetcodes questions/71.py
+++ leetcodes questions/71.py
@@ -14,3 +14,26 @@ class Solution(object):
                 curr += c
                 
         return '/' + '/'.join(stack)
+
+class Solution(object):
+    def simplifyPath(self, path):
+        stack = []
+        curr = ''
+        
+        for i in path + '/':
+            if i == '/':
+                if curr and curr != '.' and curr != '..':
+                    stack.append(curr)
+                else:
+                    if stack and curr == '..': stack.pop()
+                        
+                curr = ''
+            else:
+                curr += i
+        
+        return '/' + '/'.join(stack)
+                
+                
+                
+                
+        # '/home/../'
