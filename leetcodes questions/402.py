e89d621c409b4dbaf5de41d3c28556e1dc696fdc
merthamit <merthamit11@gmail.com>
2023-01-06 04:26:17 +0300

    402

---
+++ leetcodes questions/402.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        if len(num) == k: return '0'
+        stack = []
+        
+        for i in num:
+            while stack and stack[-1] > i and k:
+                stack.pop()
+                k -= 1
+                
+            stack.append(i)
+        
+        while k:
+            stack.pop()
+            k -= 1
+        
+        if len(stack) != 1 and stack[0] == 0:
+            stack = stack[1:]
+        
+        return str(int(''.join(stack)))
