810b7fa44223cc8ea86631fa7266219943cbeb98
merthamit <merthamit11@gmail.com>
2023-01-15 19:33:02 +0300

    139

---
+++ leetcodes questions/139.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+        
+        for i in range(len(s) -1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+                    
+        return dp[0]
