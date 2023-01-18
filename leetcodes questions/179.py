b65e795f63d05c46b1c7f195eb4b48088b5c967b
merthamit <merthamit11@gmail.com>
2023-01-18 14:25:43 +0300

    179

--- leetcodes questions/179.py
+++ leetcodes questions/179.py
@@ -10,10 +10,26 @@ class Solution(object):
                 return 1
             else:
                 return 0
-            
+
+        # Bubble Sort    
         for x in range(len(nums)-1, -1, -1):
             for y in range(x):
                 if largest(str(nums[y]), str(nums[y+1])):
                     nums[y], nums[y+1] = nums[y+1], nums[y]
                     
         return str(int(''.join(nums)))
+        
+def cmp_to_key(): return
+class Solution(object):
+    def largestNumber(self, nums):
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+            
+        def largest(a, b):
+            if a + b > b + a:
+                return -1
+            else:
+                return 1
+        
+        res = sorted(nums, key=cmp_to_key(largest))
+        return str(int(''.join(res)))
