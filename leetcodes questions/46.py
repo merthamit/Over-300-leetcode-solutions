6a92338835a79b41a7f59bb6ef8e22ad8eb93eeb
merthamit <merthamit11@gmail.com>
2023-01-18 10:59:48 +0300

    46

--- leetcodes questions/46.py
+++ leetcodes questions/46.py
@@ -1,3 +1,4 @@
+import collections
 class Solution(object):
     def permute(self, nums):
         res = []
@@ -16,4 +17,28 @@ class Solution(object):
             nums.append(n)
             
         return res
+
+class Solution(object):
+    def permute(self, nums):
+        perm, res = [], []
+        count = collections.Counter(nums)
+        
+        def dfs():
+            if len(nums) == len(perm):
+                res.append(perm[:])
+                return
+            
+            for n in count:
+                if count[n]:
+                    perm.append(n)
+                    count[n] -= 1
+                    
+                    dfs()
+                    
+                    perm.pop()
+                    count[n] += 1
+        
+        dfs()
+        return res
+    
         
