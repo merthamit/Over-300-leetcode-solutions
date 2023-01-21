bdc1fca6dd5d60bb8eac93b6bea26594e3bb7068
merthamit <merthamit11@gmail.com>
2023-01-21 13:59:21 +0300

    45

---
+++ leetcodes questions/45.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def jump(self, nums):
+        l, r = 0, 0
+        res = 0
+        
+        while r < len(nums) - 1:
+            maxJump = 0
+            
+            for i in range(l, r + 1):
+                maxJump = max(maxJump, i + nums[i])
+                
+            l = r + 1
+            r = maxJump
+            res += 1
+        
+        return res
+        
