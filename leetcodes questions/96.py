aba33a5c7b51cc49af02b3048ab2ad0799dc77fa
merthamit <merthamit11@gmail.com>
2023-01-29 22:39:57 +0300

    96

---
+++ leetcodes questions/96.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numTrees(self, n):
+        numTree = [1] * (n+1)
+        
+        for nodes in range(2, n + 1):
+            total = 0
+            for root in range(1, nodes + 1):
+                left = root - 1
+                right = nodes - root
+                total += numTree[left] * numTree[right]
+                
+            numTree[nodes] = total
+            
+        return numTree[n]
