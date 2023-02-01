4cfa0d99894322fa89331fc7765c4b7a861ab429
merthamit <merthamit11@gmail.com>
2023-02-01 18:38:05 +0300

    450

---
+++ leetcodes questions/450.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deleteNode(self, root, key):
+        if not root: return
+        
+        if root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            
+            curr.left = root.left
+            return root.right
+            
+        return root
+                
+            
+            
+        
