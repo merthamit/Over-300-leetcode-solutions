8f77d946a069c54683534795cae6e4390b1ba053
merthamit <merthamit11@gmail.com>
2023-02-02 19:27:12 +0300

    2001.

---
+++ leetcodes questions/2001.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def interchangeableRectangles(self, rectangles):
+        rectangleHash, res = {}, 0
+        
+        for w, h in rectangles:
+            rectangleHash[(float(w) / h)] = 1 + rectangleHash.get((float(w) / h), 0)
+            
+        for w, h in rectangles:
+            rectangleHash[(float(w) / h)] -= 1
+            res += rectangleHash[(float(w) / h)]
+        
+        return res
+
+class Solution(object):
+    def interchangeableRectangles(self, rectangles):
+        rectangleHash, res = {}, 0
+        
+        for w, h in rectangles:
+            rectangleHash[(float(w) / h)] = 1 + rectangleHash.get((float(w) / h), 0)
+            
+        for c in rectangleHash.values():
+            if c > 1:
+                res += (c * (c - 1)) // 2
+        
+        return res
