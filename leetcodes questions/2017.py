f9152c82f493ac61df9d70a4aa7fa77ced273cf2
merthamit <merthamit11@gmail.com>
2023-02-03 01:49:13 +0300

    2017

---
+++ leetcodes questions/2017.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def gridGame(self, grid):
+        N = len(grid[0])
+        preRow1, preRow2 = grid[0][:], grid[1][:]
+        
+        for i in range(1, N):
+            preRow1[i] += preRow1[i-1]
+            preRow2[i] += preRow2[i-1]
+        
+        res = float('inf')
+        for i in range(N):
+            top = preRow1[-1] - preRow1[i]
+            bottom = preRow2[i - 1] if i > 0 else 0
+            secondRobot = max(top, bottom)
+            res = min(res, secondRobot)
+        
+        return res
